650968b75f2184c4c01cae6747ec0cdf3581c662
Aleksander Heintz <alxandr@alxandr.me>
2025-07-21 13:59:34 +0200

    more

--- system/peers.nix
+++ system/peers.nix
@@ -2,18 +2,18 @@
 {
   # Control Plane
   wg-bgp-mesh.peers."talos-n1-cp" = {
-    port = 51821;
+    port = 51971;
     internal.ipv4 = "192.168.60.151";
     bgp.weight = 100;
   };
 
   # Worker Nodes
   wg-bgp-mesh.peers."talos-n1-w1" = {
-    port = 51822;
+    port = 51981;
     internal.ipv4 = "192.168.60.161";
   };
   wg-bgp-mesh.peers."talos-n1-w2" = {
-    port = 51823;
+    port = 51982;
     internal.ipv4 = "192.168.60.162";
     bgp.weight = 100;
   };
