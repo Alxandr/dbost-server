286bb3c1590a04054f68ae4dca481e1b71fc9db5
Aleksander Heintz <alxandr@alxandr.me>
2025-07-23 00:01:50 +0200

    frr policy

--- system/frr/config.nix
+++ system/frr/config.nix
@@ -27,6 +27,16 @@ in
 ''
   frr defaults datacenter
 
+  ! Defines a prefix-list for Talos LB address space
+  ip prefix-list talos-lb seq 5 permit 10.252.0.0/16 le 32
+
+  ! Creates a route-map that permits only Talos LB addresses
+  route-map talos-lb permit 10
+    match ip address prefix-list talos-lb
+
+  ! Deny everything else
+  route-map talos-lb deny 10
+
   router bgp ${as}
     bgp router-id ${router-id}
     bgp fast-convergence
@@ -38,6 +48,7 @@ in
       neighbor ${peer.bgp.ipv4} remote-as ${builtins.toString peer.bgp.as}
       neighbor ${peer.bgp.ipv4} soft-reconfiguration inbound
       neighbor ${peer.bgp.ipv4} weight ${builtins.toString peer.bgp.weight}
+      neighbor ${peer.bgp.ipv4} route-map talos-lb in
     '') peerList}
 
     ! IPv4 config
