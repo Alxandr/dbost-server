1e7119153fc64f81555c72ea3a4245e18acd9c52
Aleksander Heintz <alxandr@alxandr.me>
2026-09-14 01:39:14 +0200

    sanity

--- modules/netbird-relay.nix
+++ modules/netbird-relay.nix
@@ -35,18 +35,20 @@ in
       serviceConfig = {
         EnvironmentFile = [ cfg.authSecretFile ];
         StateDirectory = "netbird-relay";
-        ExecStart = "${lib.getExe netbird-relay} \
-          --listen-address=:33080 \
-          --exposed-address=rels://relay.netbird.alxandr.me:443 \
-          --enable-stun \
-          --stun-ports 3478 \
-          --stun-log-level info \
-          --log-file=console \
-          --metrics-port=9090 \
-          --health-listen-address=127.0.0.1:9000 \
-          --letsencrypt-data-dir=/var/lib/netbird-relay \
-          --letsencrypt-domains=relay.netbird.alxandr.me \
-          --letsencrypt-email=\"alxandr@alxandr.me\"";
+        ExecStart = lib.concatStringsSep " " [
+          "${lib.getExe netbird-relay}"
+          "--listen-address=:33080"
+          "--exposed-address=rels://relay.netbird.alxandr.me:443"
+          "--enable-stun"
+          "--stun-ports 3478"
+          "--stun-log-level info"
+          "--log-file=console"
+          "--metrics-port=9090"
+          "--health-listen-address=127.0.0.1:9000"
+          "--letsencrypt-data-dir=/var/lib/netbird-relay"
+          "--letsencrypt-domains=relay.netbird.alxandr.me"
+          "--letsencrypt-email=\"alxandr@alxandr.me\""
+        ];
         Restart = "always";
         User = "netbird-relay";
         Group = "netbird-relay";
